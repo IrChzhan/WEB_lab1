a92caf979588baa54eb089dcac4b03a6c64a745b
Irina Chzhan <154551246+IrChzhan@users.noreply.github.com>
2024-12-15 11:18:09 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# Вариант 12: Экспортировать в текстовый файл путь до файла истории команд (переменная окружения $HISTFILE) и его максимальный размер (переменная окружения $HISTFILESIZE). 
 echo "HISTFILE=$HISTFILE" > myscript.txt
 echo "HISTFILESIZE=$HISTFILESIZE" >> myscript.txt
